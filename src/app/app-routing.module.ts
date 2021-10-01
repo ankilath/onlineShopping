9f104ca1bd6fee665188bd817b3505391c94e8df
Ankita Lath <ankitalath21@gmail.com>
2021-10-01 14:21:07 +0530

    fixed error of home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
 
 
 const routes: Routes = [
